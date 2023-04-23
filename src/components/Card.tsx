52ddc84c9bc50eee49b29aec6eb68d9bee593706
Apocryphon06 <hrithikchandrashekar@gmail.com>
2023-04-23 23:15:57 +0530

    clean up

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,13 @@
 const Card = ({ data }: any) => {
   return (
-    <div className="bg-gray-700 rounded shadow-xl flex items-center gap-10 ">
+    <div className="bg-gray-700 rounded shadow-xl flex lg:flex-row gap-5 items-center ">
       <img
-        className="lg:w-[170px] w-[140px] h-[140px] lg:h-[170px] rounded-tl rounded-bl"
+        className="lg:w-[170px] w-[140px] h-[140px] lg:h-[170px] lg:rounded-tl lg:rounded-bl lg:p-0 p-2"
         src={data.image}
         alt={data.image}
       />
-      <div className="lg:w-[320px]">
-        <p className="lg:text-xl text-lg font-bold tracking-wide">
+      <div className="lg:w-[320px] w-full">
+        <p className="lg:text-xl text-base font-bold tracking-wide">
           {data.name}
         </p>
         <p className={`text-sm `}>
@@ -27,9 +27,9 @@ const Card = ({ data }: any) => {
           </div>
         </p>
         <p className="text-[#808080] text-sm">Last known location:</p>
-        <p className="text-base font-medium"> {data.location.name}</p>
+        <p className="text-sm font-medium"> {data.location.name}</p>
         <p className="text-[#808080] text-sm">First seen in:</p>
-        <p className="text-base font-medium"> {data.origin.name}</p>
+        <p className="text-sm font-medium"> {data.origin.name}</p>
       </div>
     </div>
   );
